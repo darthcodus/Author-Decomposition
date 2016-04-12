d74615755a1a51150be876ce5694e83926505115
Heetae Kim <jason.heetae.kim@gmail.com>
2016-04-11 20:28:20 -0700

    In development

--- src/feature.py
+++ src/feature.py
@@ -1,5 +1,65 @@
 #!/usr/bin/env python3
+import glob
+from collections import deque
+
 from lib.pycorenlp import StanfordCoreNLP
+
+
+def count_tokens(tokens):
+    assert isinstance(tokens, list)
+    model = {}
+    for token in tokens:
+        assert isinstance(token, str)
+        num = model.get(token, 0)
+        model[token] = num + 1
+    return model
+
+
+def tokenize_es(texts):
+    assert isinstance(texts, str)
+    nlp = StanfordCoreNLP('http://25.30.82.122:8011')
+    output = nlp.annotate(texts, properties={
+        "annotators": "tokenize,ssplit",
+        "coref.md.type": "dep",
+        "coref.mode": "statistical"
+    })
+
+    tokens = []
+    for sentence in output['sentences']:
+        for token in sentence['tokens']:
+            tokens.append(token['word'])
+    return tokens
+
+
+def split_sentences_es(texts):
+    assert isinstance(texts, str)
+    nlp = StanfordCoreNLP('http://25.30.82.122:8011')
+    output = nlp.annotate(texts, properties={
+        "annotators": "tokenize,ssplit",
+        "coref.md.type": "dep",
+        "coref.mode": "statistical"
+    })
+
+    sentences = []
+    for s in output['sentences']:
+        sentence = ''
+        for token in s['tokens']:
+            sentence += token['word']
+        sentences.append(sentence)
+    return sentences
+
+
+def tokenize_other(texts):
+    assert isinstance(texts, str)
+    return texts.split()
+
+
+def save_model(model, output_file):
+    assert isinstance(model, dict)
+    assert isinstance(output_file, str)
+    with open(output_file, 'w', encoding='utf-8') as file:
+        for word, num in model.items():
+            file.write(str.format('{} {}\n', word, num))
 
 
 class WordFrequency:
@@ -7,63 +67,77 @@ class WordFrequency:
         assert isinstance(lang, str)
         self.lang = lang
 
-    def build_model(self, input_file, output_file):
-        assert isinstance(input_file, str)
+    def build_model(self, input_path, output_file):
+        assert isinstance(input_path, str)
         assert isinstance(output_file, str)
+        paths = str.format('{}/*/*', input_path)
+
         texts = ''
-        with open(input_file, 'r', encoding='utf-8') as file:
-            for line in file:
-                texts += line
+        for file_path in glob.glob(paths):
+            with open(file_path, 'r', encoding='utf-8') as file:
+                for line in file:
+                    texts += line
 
-        tokens = []
         if self.lang == 'es':
-            tokens = self._tokenize_es(texts)
-        model = self._count_words(tokens)
-        self._write_file(model, output_file)
-
-    @staticmethod
-    def _tokenize_es(texts):
-        assert isinstance(texts, str)
-        nlp = StanfordCoreNLP('http://25.30.82.122:8011')
-        output = nlp.annotate(texts, properties={
-            "annotators": "tokenize,ssplit",
-            "coref.md.type": "dep",
-            "coref.mode": "statistical"
-        })
-        print(output['sentences'][0]['tokens'][1]['word'])
-
-        tokens = []
-        for sentence in output['sentences']:
-            for token in sentence['tokens']:
-                tokens.append(token['word'])
-        return tokens
-
-    @staticmethod
-    def _count_words(tokens):
-        assert isinstance(tokens, list)
-        model = {}
-        for token in tokens:
-            assert isinstance(token, str)
-            num = model.get(token, 0)
-            model[token] = num + 1
-        return model
-
-    @staticmethod
-    def _write_file(model, output_file):
-        assert isinstance(model, dict)
-        assert isinstance(output_file, str)
-        with open(output_file, 'w', encoding='utf-8') as file:
-            for word, num in model.items():
-                file.write(str.format('{} {}\n', word, num))
+            tokens = tokenize_es(texts)
+        else:
+            tokens = tokenize_other(texts)
+        model = count_tokens(tokens)
+        save_model(model, output_file)
 
 
 class NCharacterGram:
-    def __init__(self):
-        pass
+    def __init__(self, n, lang='es'):
+        assert isinstance(n, int)
+        assert isinstance(lang, str)
+        if n <= 0:
+            raise AssertionError('The parameter should be greater than 0.')
+        self.n = n
+        self.lang = lang
+
+    def build_model(self, input_path, output_file):
+        assert isinstance(input_path, str)
+        assert isinstance(output_file, str)
+        paths = str.format('{}/*/*', input_path)
+
+        chunks = []
+        texts = ''
+        for file_path in glob.glob(paths):
+            with open(file_path, 'r', encoding='utf-8') as file:
+                for line in file:
+                    if len(texts) + len(line) >= 1000000:
+                        chunks.append(texts)
+                        texts = ''
+                    texts += line
+        chunks.append(texts)
+
+        # Not tokenizing is better to capture stylistic features.
+        # if self.lang == 'es':
+        #     sentences = split_sentences_es(texts)
+        # else:
+        #     sentences = [texts]
+        ngrams = []
+        for chunk in chunks:
+            for ngram in self._make_ngram(chunk):
+                ngrams.append(ngram)
+
+        model = count_tokens(ngrams)
+        print(model)
+        save_model(model, output_file)
+
+    def _make_ngram(self, texts):
+        assert isinstance(texts, str)
+        grams = []
+        for i in range(len(texts) - self.n):
+            grams.append(texts[i:i + self.n])
+        return grams
 
 
 def main():
-    pass
+    nc = NCharacterGram(4, 'es')
+    input_file = '../corpora/spanish_blogs/alejandro nieto gonzalez/Ciertos comportamientos ecológicos puede que no lo sean.txt'
+    output_file = 'test_nc_model.txt'
+    nc.build_model(input_file, output_file)
 
 
 if __name__ == '__main__':
--- test/test_feature.py
+++ test/test_feature.py
@@ -1,18 +1,30 @@
 import os
 import unittest
 
-from src.feature import WordFrequency
+from src.feature import WordFrequency, NCharacterGram
 
 
-class WordFrequencyTest(unittest.TestCase):
+# class WordFrequencyTest(unittest.TestCase):
+#     def setUp(self):
+#         self.wf = WordFrequency(lang='es')
+#
+#     def test_build_model(self):
+#         wf = self.wf
+#         input_file = '../corpora/spanish_blogs'
+#         output_file = 'test_wq_model.txt'
+#         wf.build_model(input_file, output_file)
+#         self.assertTrue(os.path.isfile(output_file))
+
+
+class NCharacterGramTest(unittest.TestCase):
     def setUp(self):
-        self.wf = WordFrequency(lang='es')
+        self.nc = NCharacterGram(4, lang='es')
 
     def test_build_model(self):
-        wf = self.wf
-        input_file = '../corpora/spanish_blogs/alejandro nieto gonzalez/Ciertos comportamientos ecológicos puede que no lo sean.txt'
-        output_file = 'test_wq_model.txt'
-        wf.build_model(input_file, output_file)
+        nc = self.nc
+        input_path = '../corpora/spanish_blogs'
+        output_file = 'test_nc_model.txt'
+        nc.build_model(input_path, output_file)
         self.assertTrue(os.path.isfile(output_file))
 
 
