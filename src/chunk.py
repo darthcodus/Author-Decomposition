dfd97b3596630b680c24081f7b4b833915369799
Anmol Ahuja <me@anmolahuja.com>
2016-04-13 16:20:19 -0700

    Add another unimplemented method to chunk.py

--- src/chunk.py
+++ src/chunk.py
@@ -3,5 +3,10 @@ class Chunker(object):
     def __init__(self, debug = False):
         self.debug = False
 
+    # return a list of sentences
     def tokenizeIntoSentences(self, text):
-        
+        raise Exception("Not implemented")
+
+    # separate text into chunks of 'chunkSize' sentences, return a list containing lists of sentences belonging to that chunk
+    def chunk(self, text, chunkSize):
+        raise Exception("Not implemented")
