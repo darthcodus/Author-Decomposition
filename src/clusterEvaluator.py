67f10895a3d6a4446937ba0107dd6cd9d504f81c
Jlong89 <jiaolong89@gmail.com>
2016-04-14 23:26:05 -0700

    started evaluator definition

---
+++ src/clusterEvaluator.py
@@ -0,0 +1,11 @@
+class ClusterEvaluator(object):
+    def __init__(self):
+
+
+
+
+    def evaluatePurity(self, clusters):
+        '''
+        Calculates purity for each cluster
+        param: clusters
+        '''
