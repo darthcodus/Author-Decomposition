445c8345b9ce69d3b72b2bb5514dde3e925261b6
Anmol Ahuja <me@anmolahuja.com>
2016-04-14 23:42:50 -0700

    Implement k-means clustering.

--- src/authorcluster.py
+++ src/authorcluster.py
@@ -1,7 +1,10 @@
 from chunk import Chunker
 from constants import Language
 import nltk
-from word_tokenizer import WordTokenizer
+from tokenizer import Tokenizer
+
+from sklearn import cluster
+from sklearn import metrics
 
 def AuthorCluster(object):
     def __init__(self, verbose = False, lang):
@@ -14,18 +17,28 @@ def AuthorCluster(object):
         vec = [x in chunk for x in self.MostCommonWords]
         return vec
 
-    # [ 0'sen1', 1'sen2', 3'sen3' ]
+    # Input: text tokenized into sentences [ 0'sen1', 1'sen2', 3'sen3' ]
     # RET: [ [0], [1, 3] ] - > len = numClusters
     def cluster(self, text, chunkSize, numClusters):
-        wt = WordTokenizer(self.Language)
-        fdist = nltk.FreqDist(wt)
+        tokenizer = Tokenizer(self.Language)
+        fdist = nltk.FreqDist(tokenizer.tokenizeIntoWords(text))
         # TODO: the english tokenizer at least also includes punctuation, filter?
         for wordfreqpair in fdist.most_common(500): # TODO: too high for the size of our texts?s
             word = wordfreqpair[0]
             # freq = wordfreqpair[1]
             self.MostCommonWords.append(word)
 
+        featurizeVectorList = []
         chunks = self.Chunker.chunk(text, chunkSize)
         for chunk in chunks:
-            fv = self.generateFeatureVector(chunk)
+            featurizeVectorList.append(self.generateFeatureVector(chunk))
 
+        km = cluster.KMeans(n_clusters=numClusters, init='k-means++', max_iter=100, n_init=1, verbose=self.Verbose)
+        labels = km.fit_predict(featurizeVectorList)
+        ret = [[]] * numClusters
+        sentencenum = 0
+        for i, label in enumerate(labels):
+            for sentence in tokenizer.tokenizeIntoSentences(chunks[i]):
+                ret[label].append(sentencenum)
+                sentencenum += 1
+        return ret
