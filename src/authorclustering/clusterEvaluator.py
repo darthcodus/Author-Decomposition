76962d7f283fe104d608ba71c45034bf1021f6f2
Anmol Ahuja <me@anmolahuja.com>
2016-04-17 01:31:43 -0700

    Bug fixing and refactoring of ClusterEvaluator, main. Evaluation still bugged.

--- src/authorclustering/clusterEvaluator.py
+++ src/authorclustering/clusterEvaluator.py
@@ -1,49 +1,40 @@
 from __future__ import division
 
-
-
 class ClusterEvaluator:
-    def __init__(self):
+    def __init__(self, verbose = False):
+        self.Verbose = verbose
         return
 
-
     def evaluatePurity(self, clusters, text):
+        #print(type(text))
         '''
         Calculates purity for each cluster
-        param: clusters -> return list from authorCluster
-        text: pickledText containing sentence/author data
+        :param: clusters -> return list from authorCluster
+        :param: text: Text object containing sentence/author data
+        :return: tuple (list of cluster majority authors, list of cluster purity values)
         '''
-        out_text = open('cluster_evaluation_output.txt', "w")
-        #count author classes in cluster
+
+        # count author classes in cluster
         cluster_class_counts = {}
-        for clus in clusters:
+        for i, clus in enumerate(clusters):
             for sent in clus:
                 #cur_class = sent.auth #TODO obtain author class from cur sentence
-                author_index = text.getAuthorIndexforSentence(sent)
-                if clus not in cluster_class_counts:
-                    cluster_class_counts[clus] = {}
-                if author_index not in cluster_class_counts[clus]:
-                    cluster_class_counts[clus][author_index] = 1
-                cluster_class_counts[clus][author_index] = cluster_class_counts[clus][author_index] + 1
+                author_index = text.getAuthorIndexForSentence(sent)
+                if i not in cluster_class_counts:
+                    cluster_class_counts[i] = {}
+                if author_index not in cluster_class_counts[i]:
+                    cluster_class_counts[i][author_index] = 1
+                cluster_class_counts[i][author_index] += 1
 
-        cluster_labels = {}
-
-        #assign majority class label to cluster
-        for cluster, class_counts in cluster_class_counts.iteritems():
-            total_count=0;
-            majority_count = 0;
-            majority_class = '';
-            for cur_class, count in class_counts.iteritems():
-                if majority_count < count:
-                    majority_count = count;
-                    majority_class = cur_class;
-
-                total_count = total_count+count
-
+        clusterMajorityAuthorIndices = []
+        clusterPurities = []
+        # assign majority class label to cluster
+        for clusterIdx, class_counts in cluster_class_counts.items():
+            total_count = sum(class_counts.values())
+            majority_class = max(class_counts, key = lambda k: class_counts[k])
+            majority_count = class_counts[majority_class]
             class_purity = majority_count/total_count
+            clusterMajorityAuthorIndices.append(majority_class)
+            clusterPurities.append(class_purity)
 
-            out_text.write("Cluster {0}: authorIdx = {1}, purity = {2}\n".format(str(cluster), str(majority_class), str(purity)))
-
-
-if __name__ == '__main__':
-    main()
+        return clusterMajorityAuthorIndices, clusterPurities
--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ def main():
     parser.add_argument('-pt','--pickledtext', help='path to a pickled text object', required=True)
     parser.add_argument('-nc', '--numclusters', help='number of author clusters', required=True)
     parser.add_argument('-chunk', '--chunksize', help='number of sentences per chunk', required=False, default = 20)
+    parser.add_argument('-op', '--outputfile', help='number of sentences per chunk', required=False, default = 'cluster_evaluation_output.txt')
     args = parser.parse_args()
     text = Text.loadFromFile(args.pickledtext)
     nc = args.numclusters
@@ -17,6 +18,13 @@ def main():
     ce = ClusterEvaluator()
     clusters = ac.cluster(text, int(args.chunksize), int(nc))
 
-    ce.evaluatePurity(clusters, text)
+    clusterMajorityAuthorIndices, clusterPurities = ce.evaluatePurity(clusters, text)
+    assert len(clusterMajorityAuthorIndices) == len(clusterPurities)
+    with open(args.outputfile, "w") as fout:
+        for clusterIdx in range(0, len(clusterMajorityAuthorIndices)):
+            majority_class = clusterMajorityAuthorIndices[clusterIdx]
+            class_purity = clusterPurities[clusterIdx]
+            fout.write("Cluster {0}: authorIdx = {1}, purity = {2}\n".format(str(clusterIdx), str(majority_class), str(class_purity)))
+            print("Cluster {0}: authorIdx = {1}, purity = {2}\n".format(str(clusterIdx), str(majority_class), str(class_purity)))
 
 main()
